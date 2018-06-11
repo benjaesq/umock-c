f02dfe425181971b72cbcfb19dfbd9f392313908
Mahesh Rohera <marohera@microsoft.com>
2018-06-10 22:59:45 -0700

    Addressing PR comments

--- src/umock_log.c
+++ src/umock_log.c
@@ -8,15 +8,12 @@
 #include "umock_log.h"
 #include "umock_c_internal.h"
 
-#if defined(__GNUC__) || defined(__clang__)
-#pragma GCC diagnostic ignored "-Wformat-nonliteral"
-#endif
 void UMOCK_LOG(const char* format, ...)
 {
     va_list params;
     va_start(params, format);
-    (void)vprintf(format, params); 
+    (void)vprintf(format, params);
     va_end(params);
-    printf("\r\n");
+    (void)printf("\r\n");
     umock_c_indicate_error(UMOCK_C_ERROR);
 }
