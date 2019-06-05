8036393f86569917d1f7352279e470678cb90ed2
Dan Cristoloveanu <dcristo@microsoft.com>
2019-06-05 11:36:38 -0700

    Add MOCKABLE_FUNCTION_WITH_CODE (temporary solution, do not use for long term)

--- inc/umock_c/umock_c_prod.h
+++ inc/umock_c/umock_c_prod.h
@@ -1,8 +1,12 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+#include "azure_macro_utils/macro_utils.h"
+
 #undef MOCKABLE_FUNCTION
 #undef MOCKABLE_FUNCTION_WITH_RETURNS
+#undef MOCKABLE_FUNCTION_WITH_CODE
+#undef MOCKABLE_FUNCTION_WITH_CODE_END
 
 /* This header is meant to be included by production code headers, so that the MOCKABLE_FUNCTION gets enabled. */
 /* 
@@ -42,24 +46,42 @@
 #define MOCKABLE_FUNCTION_WITH_RETURNS(modifiers, result, function, ...) \
     MU_IF(ENABLE_MOCK_FILTERING_SWITCH,MU_IF(MU_C2(please_mock_, function),MOCKABLE_FUNCTION_DISABLED,MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK), MOCKABLE_FUNCTION_UMOCK_INTERNAL_WITH_MOCK) (1, modifiers, result, function, __VA_ARGS__)
 
+// The below MOCKABLE_FUNCTION_WITH_CODE macros are a temporary solution and should not be used for long term
+// They will be removed once the real support is in umock_c
+#define MOCKABLE_FUNCTION_WITH_CODE(modifiers, result, function, ...) \
+    MOCKABLE_FUNCTION_INTERNAL_WITH_CODE(modifiers, result, function, __VA_ARGS__)
+
+#define MOCKABLE_FUNCTION_WITH_CODE_END() \
+    MOCK_FUNCTION_END()
+
 #include "umock_c/umock_c.h"
 
 #else
 
-#include "azure_macro_utils/macro_utils.h"
-
 #define UMOCK_C_PROD_ARG_IN_SIGNATURE(count, arg_type, arg_name) arg_type arg_name MU_IFCOMMA(count)
+
+#define MOCKABLE_FUNCTION_SIGNATURE(modifiers, result, function, ...) \
+    result modifiers function(MU_IF(MU_COUNT_ARG(__VA_ARGS__),,void) MU_FOR_EACH_2_COUNTED(UMOCK_C_PROD_ARG_IN_SIGNATURE, __VA_ARGS__))
 
 /* Codes_SRS_UMOCK_C_LIB_01_002: [The macro shall generate a function signature in case ENABLE_MOCKS is not defined.] */
 /* Codes_SRS_UMOCK_C_LIB_01_005: [If ENABLE_MOCKS is not defined, MOCKABLE_FUNCTION shall only generate a declaration for the function.] */
 /* Codes_SRS_UMOCK_C_LIB_01_001: [MOCKABLE_FUNCTION shall be used to wrap function definition allowing the user to declare a function that can be mocked.]*/
 #define MOCKABLE_FUNCTION(modifiers, result, function, ...) \
-    result modifiers function(MU_IF(MU_COUNT_ARG(__VA_ARGS__),,void) MU_FOR_EACH_2_COUNTED(UMOCK_C_PROD_ARG_IN_SIGNATURE, __VA_ARGS__));
+    MOCKABLE_FUNCTION_SIGNATURE(modifiers, result, function, __VA_ARGS__);
 
 /* Codes_SRS_UMOCK_C_LIB_01_213: [ The macro shall generate a function signature in case `ENABLE_MOCKS` is not defined. ]*/
 /* Codes_SRS_UMOCK_C_LIB_01_212: [ `MOCKABLE_FUNCTION_WITH_RETURNS` shall be used to wrap function definitions, allowing the user to declare a function that can be mocked and aditionally declares the values that are to be returned in case of success and failure. ]*/
 #define MOCKABLE_FUNCTION_WITH_RETURNS(modifiers, result, function, ...) \
     result modifiers function(MU_IF(MU_COUNT_ARG(__VA_ARGS__),,void) MU_FOR_EACH_2_COUNTED(UMOCK_C_PROD_ARG_IN_SIGNATURE, __VA_ARGS__)); \
     MU_IF(UMOCK_C_PROD_IS_NOT_VOID(result), DO_NOTHING_WITH_RETURN_VALUES,)
+
+#define UMOCK_C_PROD_ARG_IN_SIGNATURE_2(count, arg_type, arg_name) 
+
+// The below MOCKABLE_FUNCTION_WITH_CODE macros are a temporary solution and should not be used for long term
+// They will be removed once the real support is in umock_c
+#define MOCKABLE_FUNCTION_WITH_CODE(modifiers, result, function, ...) \
+    MOCKABLE_FUNCTION_SIGNATURE(modifiers, result, function, __VA_ARGS__)
+
+#define MOCKABLE_FUNCTION_WITH_CODE_END() \
 
 #endif
